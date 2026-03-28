a2fb359fc0b515ccfb1bfeec41ab463edb66064b
rupamghosh2006 <rupamgh2006@gmail.com>
2026-03-28 10:55:30 +0530

    add variables demonstration in variables.go

---
+++ 3_variables/variables.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	//var name string = "golang"
+
+	//infer
+	//var name = "golang"
+
+	//shorthand syntax
+
+	name := "golang"
+
+	fmt.Println(name)
+}
